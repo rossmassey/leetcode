0b021d3a9da5be615a9d978b13eda8524c3fd4b2
Ross Massey <massey0ross@gmail.com>
2023-05-01 00:17:19 -0700

    add 104

---
+++ Sources/leetcode/104/MaximumDepthOfBinaryTree.swift
@@ -0,0 +1,18 @@
+import common
+
+/**
+ 104 - Maximum Depth of Binary Tree - Easy
+ 
+ Runtime:    O(n)
+ Spacetime:  O(n)
+ 
+ */
+struct Solution104 {
+
+    typealias TreeNode = common.TreeNode
+
+    static func maxDepth(_ root: TreeNode?) -> Int {
+        guard let root = root else { return 0 }
+        return max(maxDepth(root.left), maxDepth(root.right)) + 1
+    }
+}
---
+++ Tests/leetcodeTests/104/MaximumDepthOfBinaryTreeTests.swift
@@ -0,0 +1,19 @@
+import XCTest
+import common
+@testable import leetcode
+
+final class MaximumDepthOfBinaryTreeTests : XCTestCase {
+    
+    let maxDepth = Solution104.maxDepth
+    typealias TreeNode = common.TreeNode
+
+    func testDepthThreeTree() {
+        let input = common.arrayToTree([3,9,20,nil,nil,15,7])
+        XCTAssertEqual(maxDepth(input), 3)
+    }
+
+        func testDepthTwoTree() {
+        let input = common.arrayToTree([1,nil,2])
+        XCTAssertEqual(maxDepth(input), 2)
+    }
+}
