a76d7cb7af3481067d11cfbd509a6048441e8663
Ross Massey <massey0ross@gmail.com>
2023-05-04 18:40:11 -0700

    add 42

--- README.md
+++ README.md
@@ -29,7 +29,7 @@ Swift solutions for leetcode problems, following [Neetcode's Algorithm Roadmap](
 |✅| 167 | 🟨 | Two Sum II Input Array Is Sorted | [src](./Sources/leetcode/167/TwoSum2InputArrayIsSorted.swift) | [url](https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/) |
 |✅| 15 | 🟨 | 3Sum | [src](./Sources/leetcode/15/ThreeSum.swift) | [url](https://leetcode.com/problems/3sum/) |
 |✅| 11 | 🟨 | Container With Most Water | [src](./Sources/leetcode/11/ContainerWithMostWater.swift) | [url](https://leetcode.com/problems/container-with-most-water/) |
-|❌| 42 | 🔺 | Trapping Rain Water |  | [url](https://leetcode.com/problems/trapping-rain-water/) |
+|✅| 42 | 🔺 | Trapping Rain Water | [src](./Sources/leetcode/42/TrappingRainWater.swift) | [url](https://leetcode.com/problems/trapping-rain-water/) |
 
 | | | | Stack | | |
 | -- | -- | -- | -- | -- | -- |
---
+++ Sources/leetcode/42/TrappingRainWater.swift
@@ -0,0 +1,43 @@
+/**
+    42 - Trapped Rain Water - Hard
+    
+    Runtime:    O(n)
+    Space:      O(1)
+    
+    use two variables `leftMax` and `rightMax` to track bounding heights
+    use two pointers to only need one pass, and no additional data structure
+ 
+    converge towards middle based on lesser height
+    use tallest height found on lesser height's side to determine water amount
+    subtract the current height, since water only on top of it
+    
+*/
+struct Solution42 {
+    static func trap(_ height: [Int]) -> Int {
+        
+        var left = 0
+        var right = height.count - 1
+
+        var leftMax = height[left]
+        var rightMax = height[right]
+
+        var trappedWater = 0
+
+        while left < right {
+            if height[left] < height[right] {
+                leftMax = max(height[left], leftMax)
+                trappedWater += leftMax - height[left]
+
+                left += 1
+            } else {
+                rightMax = max(height[right], rightMax)
+                trappedWater += rightMax - height[right]
+
+                right -= 1
+            }
+        }
+
+        return trappedWater
+        
+    }
+}
