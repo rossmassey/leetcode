00f66b30fe9c42ec4d495c01d263891f5a71e716
Ross Massey <massey0ross@gmail.com>
2023-05-08 14:56:57 -0700

    add 1 tests

--- Tests/leetcodeTests/1/TwoSumTests.swift
+++ Tests/leetcodeTests/1/TwoSumTests.swift
@@ -1,7 +1,26 @@
 import XCTest
+import common
 @testable import leetcode
 
 final class TwoSumTests : XCTestCase {
     
-// TODO
+    let twoSum = Solution1.twoSum
+    
+    func testLengthFourArray() {
+        let expectedOutput = anyOrderArray([0,1])
+        let output = anyOrderArray(twoSum([2,7,11,15], 9))
+        XCTAssertEqual(expectedOutput, output)
+    }
+    
+    func testLengthThreeArray() {
+        let expectedOutput = anyOrderArray([1,2])
+        let output = anyOrderArray(twoSum([3,2,4], 6))
+        XCTAssertEqual(expectedOutput, output)
+    }
+    
+    func testLengthTwoArray() {
+        let expectedOutput = anyOrderArray([0,1])
+        let output = anyOrderArray(twoSum([3,3], 6))
+        XCTAssertEqual(expectedOutput, output)
+    }
 }
