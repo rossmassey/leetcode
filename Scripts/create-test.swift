40dafbe00d256d83ec70fa28414b18ddbfd43698
Ross Massey <massey0ross@gmail.com>
2023-05-08 18:55:38 -0700

    update test template

--- Scripts/create-test.swift
+++ Scripts/create-test.swift
@@ -9,6 +9,24 @@ func loadTemplate() -> String? {
     return try? String(contentsOf: templateURL)
 }
 
+func loadSolutionFile() -> String? {
+    let inputFilePath = CommandLine.arguments[1]
+    let inputFileURL = URL(fileURLWithPath: inputFilePath)
+    return try? String(contentsOf: inputFileURL)
+}
+
+func extractFunctionName(from solutionContent: String) -> String? {
+    let pattern = "static func ([a-zA-Z0-9_]+)\\("
+    let regex = try? NSRegularExpression(pattern: pattern, options: [])
+    
+    if let match = regex?.firstMatch(in: solutionContent, options: [], range: NSRange(location: 0, length: solutionContent.utf16.count)) {
+        if let range = Range(match.range(at: 1), in: solutionContent) {
+            return String(solutionContent[range])
+        }
+    }
+    return nil
+}
+
 func createTest() {
     guard CommandLine.arguments.count == 2 else {
         print("Usage: create-test.swift <path-to-file>")
@@ -34,10 +52,21 @@ func createTest() {
         print("Error loading template")
         exit(1)
     }
+    
+    guard let solutionContent = loadSolutionFile() else {
+        print("Error loading solution file")
+        exit(1)
+    }
+    
+    guard let functionName = extractFunctionName(from: solutionContent) else {
+        print("Error extracting function name from the solution file")
+        exit(1)
+    }
 
     let contents = template
         .replacingOccurrences(of: "{number}", with: number)
         .replacingOccurrences(of: "{basename}", with: basename)
+        .replacingOccurrences(of: "{function}", with: functionName)
 
     do {
         try contents.write(to: outputFileURL, atomically: true, encoding: .utf8)
