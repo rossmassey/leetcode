55c5a0247768a6620dce45f0ceddf367a0d9a1d1
Ross Massey <massey0ross@gmail.com>
2023-05-25 01:13:39 -0700

    update 20

--- Sources/leetcode/0020-ValidParentheses.swift
+++ Sources/leetcode/0020-ValidParentheses.swift
@@ -5,6 +5,16 @@ struct Solution20 {
     /**
      Runtime:    `O(n)`
      Space:      `O(n)`
+
+     checks string's parentheses validity using stack
+
+     requires even string length
+     
+     pushes opening brackets to stack
+     checks and pops stack if closing brackets match
+     returns false if they don't
+     
+     returns true if stack empty after all characters processed
      */
     static func isValid(_ s: String) -> Bool {
         guard s.count % 2 == 0 else { return false }
@@ -17,13 +27,12 @@ struct Solution20 {
         
         var stack = [Character]()
         
-        for brace in s {
-            if let openingBrace = pairs[brace] {
-                if stack.isEmpty || openingBrace != stack.removeLast() {
-                    return false
-                }
+        for bracket in s {
+            if let openingBracket = pairs[bracket], !stack.isEmpty, openingBracket == stack.last {
+                stack.removeLast()
+            } else {
+                stack.append(bracket)
             }
-            else { stack.append(brace) }
         }
         
         return stack.isEmpty
