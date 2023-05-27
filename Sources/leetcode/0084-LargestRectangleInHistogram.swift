1a73ce9c2e4cfe417b14e7e7d50c8e719c04b230
Ross Massey <massey0ross@gmail.com>
2023-05-26 23:27:34 -0700

    update 84

--- Sources/leetcode/0084-LargestRectangleInHistogram.swift
+++ Sources/leetcode/0084-LargestRectangleInHistogram.swift
@@ -7,45 +7,54 @@ struct Solution84 {
      Runtime:    `O(n)`
      Space:      `O(n)`
     
-     use a `stack` to track heights less than current one
+     1. For each bar in the histogram, compare the bar's height (`currentHeight`) with the `height` of the `edge` at the `top` of the `stack`
 
-     at each `height`, if the `height` at the `top` of `stack` is `>= currentHeight`,
-     then the current height will be the limiting height for any future rectangles
+     2. If the current bar is shorter or equal to the `edge` on the `stack`, start removing from the `stack`. This process involves forming rectangles with each removed `edge`. The height of the rectangle is the height of the removed `edge`, and the width is the distance between the index of the current bar (`currentIndex`) and the start index of the removed `edge` (`edge.start`)
 
-     so we keep popping the stack until we find a `height` which is smaller than the `currentHeight`
+     3. Find rectangle area for each removed `edge` using its `height` and calculated `width`, and update `largestArea` if the area of the current rectangle is larger
 
-     width of each rectangle is determined by how far it extends to the right in the histogram
-     which is found using `currentIndex - element.start`
+     4. After removing taller bars, add the current bar as  an `edge` to the `stack`, noting its starting point (`startOfCurrentHeight`). This `edge` will mark start of a new rectangle whose height was determined by this bar
 
-     since  we know `element.height` from `stack` is greater than `currentHeight`
-     we can extend out `leftMostIndex` to `element.start`
+     5. Once all bars are processed, the remaining `edges` in the `stack` are the ones without a shorter bar to their right. These bars represent rectangles that span all the way to the end of the histogram
 
-     once we are at point where `stack` is empty
-     or `top` of `stack` is `< currentHeight`
-     then add to stack `currentHeight` and how far height extends to left
+     6. For each remaining `edge` in the stack, extend their area to the end of the histogram and update `largestArea` if the area of this rectangle is larger
 
-     after one pass, each remaining `element` in the `stack` represents a `height`
-     for which we haven't found a smaller one to its right,
-     so its corresponding rectangle can be extended till the end of the histogram
+     7. The function returns the `largestArea` found
 
-     iterate through remaining to update `maxArea` if needed (such as  case when `heights` in increasing order)
+     This process ensures all possible rectangle areas under the histogram are checked, and the largest one is found.
     */
     static func largestRectangleArea(_ heights: [Int]) -> Int {
-        var maxArea = 0
-        var stack = [(start: Int, height: Int)]()
+        var largestArea = 0
+
+        // "left edge" of rectangle stored in stack
+        var stack = [(start: Int, height: Int)]() 
 
         for (currentIndex, currentHeight) in heights.enumerated() {
-            var leftMostIndex = currentIndex
+            var startOfCurrentHeight = currentIndex
+
+            // want to maintain an monotonic increasing stack
+            // so when currentHeight breaks the order:
+            // find previous areas taller than currentHeight for max
+            // since going forward we are limited by currentHeight
             while let top = stack.last, top.height >= currentHeight {
-                let element = stack.removeLast()
-                maxArea = max(maxArea, element.height * (currentIndex - element.start))
-                leftMostIndex = element.start
+                let edge = stack.removeLast()
+                let width = currentIndex - edge.start
+                largestArea = max(largestArea, edge.height * width)
+                
+                // this edge is >= currentHeight
+                // so rectangle with currentHeight can extend to this edge
+                startOfCurrentHeight = edge.start
             }
-            stack.append((leftMostIndex, currentHeight))
+            stack.append((startOfCurrentHeight, currentHeight))
         }
         
-        return stack.reduce(into: maxArea) { maxArea, element in
-            maxArea = max(maxArea, element.height * (heights.count - element.start))
+        // these edges did not have bars lower than them
+        // so their area extend to end of histogram
+        for edge in stack {
+            let width = heights.count - edge.start
+            largestArea = max(largestArea, edge.height * width)
         }
+
+        return largestArea
     }
 }
