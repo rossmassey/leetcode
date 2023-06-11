a4529cd1f7fecccc0625338701fc2cf93bd7f71b
Ross Massey <massey0ross@gmail.com>
2023-06-11 00:28:26 -0700

    update 981

--- Sources/leetcode/0981_TimeBasedKeyValueStore.swift
+++ Sources/leetcode/0981_TimeBasedKeyValueStore.swift
@@ -20,23 +20,21 @@ public class Solution_0981 {
     
     var timeMap: [String : [(value: String, time: Int)]]
 
-    init() {
+    public init() {
         timeMap = [:]
     }
     
     /**
      Runtime:    `O(1)`
-     Space:      `O(1)`
+     Space:      `O(n)`
      
      all `timestamp` given will be strictly increasing (like unix time)
      
      so this will keep our `times` array sorted after each insert
      
      otherwise a `O(log n)` insert would be required to main sorted property
-     
-     while this operation is `O(1)` space, `timeMap` has overall space complexity of `O(n)`
      */
-    func set(_ key: String, _ value: String, _ timestamp: Int) {
+    public func set(_ key: String, _ value: String, _ timestamp: Int) {
         timeMap[key, default: []].append((value, timestamp))
     }
     
@@ -48,7 +46,7 @@ public class Solution_0981 {
      
      each `times` array of tuples in our `timeMap` is already sorted, to do how `set` is called
      */
-    func get(_ key: String, _ timestamp: Int) -> String {
+    public func get(_ key: String, _ timestamp: Int) -> String {
         guard let times = timeMap[key] else { return "" }
                 
         var closestValueBelowTimestamp = ""
