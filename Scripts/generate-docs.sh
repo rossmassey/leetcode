2b5949e66657003c26f93f89560bf576af9f537d
Ross Massey <massey0ross@gmail.com>
2023-06-16 18:34:23 -0700

    add source service to docc

--- Scripts/generate-docs.sh
+++ Scripts/generate-docs.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+pwd # adding for testing 
 
 swift package --allow-writing-to-directory ./_site \
     generate-documentation --target leetcode \
     --disable-indexing \
     --transform-for-static-hosting \
     --hosting-base-path leetcode \
-    --output-path ./_site
+    --output-path ./_site \
+    --source-service github \
+    --source-service-base-url https://github.com/rossmassey/leetcode/blob/main \
+    --checkout-path /home/runner/work/leetcode/leetcode # absolute path of checked out reposistory
--- Scripts/preview-docs.sh
+++ Scripts/preview-docs.sh
@@ -2,4 +2,7 @@
 
 swift package --disable-sandbox \
     preview-documentation \
-    --target leetcode
+    --target leetcode \
+    --source-service github \
+    --source-service-base-url https://github.com/rossmassey/leetcode/blob/main \
+    --checkout-path /Users/ross/Developer/leetcode # absolute path of checked out reposistory
