3d91620b8ddbd0b04771eeefc38a12bff974160a
Ross Massey <massey0ross@gmail.com>
2023-08-22 18:31:15 -0700

    update 155

--- Sources/leetcode/0155_MinStack.swift
+++ Sources/leetcode/0155_MinStack.swift
@@ -19,15 +19,19 @@
  - At most `3 * 10^4` calls will be made to `push`, `pop`, `top`, and `getMin`.
  */
 public class Solution_0155 {
-    /// Space:    `O(1)`
+
     var stack: [(val: Int,min: Int?)]
     var min: Int? 
     
+    /// Overall Space Complexity: `O(n)`
     public init() {
         stack = [(val: Int,min: Int?)]()
     }
     
-    /// Runtime:    `O(1)` Space: `O(n)`
+    /**
+     Runtime:    `O(1)`
+     Space:      `O(1)`
+     */
     public func push(_ val: Int) {
         if min == nil || val < min! {
             min = val
@@ -35,18 +39,27 @@ public class Solution_0155 {
         stack.append((val,min))
     }
     
-    /// Runtime:    `O(1)`
+    /**
+     Runtime:    `O(1)`
+     Space:      `O(1)`
+     */
     public func pop() {
         stack.removeLast()
         min = stack.last?.min
     }
     
-    /// Runtime:    `O(1)`
+    /**
+     Runtime:    `O(1)`
+     Space:      `O(1)`
+     */
     public func top() -> Int {
         return stack.last!.val
     }
     
-    /// Runtime:    `O(1)`
+    /**
+     Runtime:    `O(1)`
+     Space:      `O(1)`
+     */
     public func getMin() -> Int {
         return min!
     }
